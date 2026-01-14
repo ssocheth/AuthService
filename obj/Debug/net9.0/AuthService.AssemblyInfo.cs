3c00a106ab5e2406116b134c85771eed2c335bcb
Sunsocheth Sorn <ssocheth@gmail.com>
2026-01-14 14:08:14 -0800

    add github action for code scan and depency scan

--- obj/Debug/net9.0/AuthService.AssemblyInfo.cs
+++ obj/Debug/net9.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928e277fc545dc91d747b0ee0d83889e01f4c681")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b10f40a619c3cc33813b13e15f9b2156e0d13a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
